74dc04cb54e98efac7223462eb3d6c5385643028
Maxime DEFRETIN <maxime.defretin@laposte.net>
2017-12-13 12:10:12 +0100

    Exercice 6

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 6 : Partie 3</title>
+  </head>
+  <body>
+    <?php
+    for ($number = 20; $number >= 0; $number--){
+    echo 'C\'est presque bon.' . ' ';
+  }
+    ?>
+  </body>
+</html>
